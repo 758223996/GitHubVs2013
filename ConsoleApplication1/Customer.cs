38487c463491b5cf3aaadd070cdca91206f87cf8
758223996@qq.com <758223996@qq.com>
2015-09-11 14:00:43 +0800

    Add Customer Class!

---
+++ ConsoleApplication1/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Customer
+    {
+        public static void Say()
+        {
+            Console.WriteLine("你好");
+        }
+
+    }
+}
